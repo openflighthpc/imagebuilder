14100dac6ad74dd82b923c5067d8a26dab71eae9
Stevie <steve.norledge@alces-flight.com>
2020-10-26 11:44:22 +0000

    fix for 8

--- platform/generic/platform.bash
+++ platform/generic/platform.bash
@@ -7,8 +7,19 @@ cat <<EOF > /etc/dracut.conf.d/11-ahci.conf
 add_drivers+=" ahci "
 EOF
 
+if [ ${DISTROMAJOR} -eq 8 ]; then
+  cat <<EOF > /etc/dracut.conf.d/11-virtio.conf
+add_drivers+=" virtio_console virtio_blk virtio_net virtio_scsi "
+EOF
+else
+  cat <<EOF > /etc/dracut.conf.d/11-virtio.conf
+add_drivers+=" virtio virtio_pci virtio_blk virtio_net virtio_scsi virtio_ring "
+EOF
+fi
+
+
 # Recreate initramfs
-KVER=$(echo /boot/vmlinuz-3* | cut -f2- -d'-')
+KVER=$(echo /boot/vmlinuz-*.x86_64 | cut -f2- -d'-')
 dracut -f /boot/initramfs-${KVER}.img ${KVER}
 
 echo "Listing initrd for reference" 
